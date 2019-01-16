8ddc971dbfbba9ef1546b2d11232c0a8bc57dcef
SaphiraNgocThuy <saphira.ngoc.thuy@gmail.com>
2019-01-16 22:10:46 +0800

    resolve conflicts

---
+++ ADProject_Team10/DeptRep/test 6.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="test 6.aspx.cs" Inherits="ADProject_Team10.DeptRep.test_6" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ ADProject_Team10/DeptRep/test 6.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADProject_Team10.DeptRep
+{
+    public partial class test_6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
